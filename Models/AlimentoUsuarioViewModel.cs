80299af10165b03679e9f32bee468e7be33918a9
teste1 <raphaelfreitassantos7@gmail.com>
2024-05-20 11:33:29 -0300

    Atualização V3.9.1

---
+++ Models/AlimentoUsuarioViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public class AlimentoUsuarioViewModel
+    {
+        public List<AlimentoModel> Alimento { get; set; }
+        public UsuarioModel Usuario { get; set; }
+    }
+}
